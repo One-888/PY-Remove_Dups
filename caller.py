2747c1c16b792ccdbc3da0e7f0cd2c43424323fe
One-888 <53633937+One-888@users.noreply.github.com>
2023-03-08 01:55:48 -0500

    add caller.py

--- caller.py
+++ caller.py
@@ -1,4 +1,11 @@
 from remove_dups import main
 
 
-main (r'C:\FFOutput\Temp\1',r'C:\FFOutput\results','MOV')
+main(
+    r"F:\All Photos\iPhone_A\iPhone_Mei_Raw\2018",
+    r"C:\FFOutput\results",
+    "MOV",
+    "SHA1",
+    1_000_000,
+    10_000_000,
+)

7bb59a9895806f2827ce8ddbca34a9a2e578e471
One-888 <53633937+One-888@users.noreply.github.com>
2023-03-11 01:53:33 -0500

    bud fix hash name looping if

--- remove_dups.py
+++ remove_dups.py
@@ -35,39 +35,46 @@ def main(
     os.chdir(destination_path)
     file_type_ = "." + file_type.lower()
 
-    file_list = glob.glob(f"{source_path}/**/*{file_type_}", recursive=True)
+    file_list = glob.glob(f"{source_path}/**/IMG_????{file_type_}", recursive=True)
     total_file_list = len(file_list)
 
     mode = input("Enter Mode(list or copy): ")
 
     for i, old_file in enumerate(file_list):
         file_size = os.stat(old_file).st_size
-        pct = (i / total_file_list) * 100
+        pct = i / total_file_list
 
-        clip = VideoFileClip(old_file)
-        duration_sec = clip.duration
+        old_name = os.path.basename(old_file).replace(file_type_, "")
+        hash_text = make_hash(filename=old_file, hashtype=hash_type)
+        short_hash_text = hash_text[0:7]
+        new_file = ("_n_" + short_hash_text + file_type_).upper()
+        new_full_path = destination_path + "\\" + new_file
 
-        if min_size < file_size < max_size and 5 < duration_sec < 60:
-            old_name = os.path.basename(old_file).replace(file_type_, "")
-            hash_text = make_hash(filename=old_file, hashtype=hash_type)
+        try:
+            clip = VideoFileClip(old_file)
+            duration_sec = clip.duration
 
-            short_hash_text = hash_text[0:7]
-            new_file = ("_n_" + short_hash_text + file_type_).upper()
+        except:
+            print(
+                f"          Skip {i}. NO TIME {pct:.0%} of {total_file_list:>} | Size: {file_size} | File: {old_file}"
+            )
+            duration_sec = 0
+
+        if 5 < duration_sec < 30:  # min_size < file_size < max_size and
             # print(file_size)
 
-            if mode == "list":
-                print(
-                    f"List: {i}. {pct:.0f}pct of {total_file_list} HASH | {hash_type}: {hash_text[0:7]} | Second: {duration_sec} | New: {new_file} | Old: {old_name} | Size: {file_size}"
-                )
-            else:
-                print(
-                    f"Copy: {i}. {pct:.0f}pct of {total_file_list} HASH | {hash_type}: {hash_text[0:7]} | Second: {duration_sec} | New: {new_file} | Old: {old_name} | Size: {file_size}"
-                )
-
+            if mode == "l":
+                mode_flag = "List"
+            elif mode == "c":
+                mode_flag = "Copy"
                 try:
-                    shutil.copy(old_file, new_file)
+                    shutil.copy(old_file, new_full_path)
                 except:
-                    print(print(f"File: {new_file} already exists. Skip it"))
+                    print(f"File: {new_file} already exists. Skip it")
+
+            print(
+                f"{mode_flag} {i}. {pct:.0%} of {total_file_list:,>} | {hash_type:>}: {hash_text[0:7]} | New: {new_file:>} | Old: {old_name} | Size: {file_size:,} | {old_file} | Sec: {duration_sec:0.0f} "
+            )
 
 
 if __name__ == "__main__":
